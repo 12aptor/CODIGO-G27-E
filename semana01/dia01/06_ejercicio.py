db330ca292b2838b767870c88357072dd20e1d1c
12aptor <paholo2054@gmail.com>
2025-10-30 23:04:46 -0500

    first commit

---
+++ semana01/dia01/06_ejercicio.py
@@ -0,0 +1,6 @@
+"""Crea una calculadora de operaciones aritméticas
+(+, -, *, /). El programa debe pedir al usuario que ingrese
+dos números y luego mostrar el resultado de la operación."""
+
+numero1 = input("Primer número: ")
+print(10 + int(numero1))
