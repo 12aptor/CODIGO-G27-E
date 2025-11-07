b75e9f7f9a52a960006430a080c73cf9f7edc845
12aptor <paholo2054@gmail.com>
2025-11-06 21:25:42 -0500

    ejercicio while

--- semana01/dia02/07_funciones.py
+++ semana01/dia02/07_funciones.py
@@ -1,4 +1,27 @@
 def sumar(a, b):
     return a + b
 
-print(sumar(1, 2))
+# resultado = sumar(5, 10)
+# print(resultado)
+
+""" Funciones con valores por defecto """
+def saludar(nombre="invitado"):
+    print(f"¡Hola {nombre}!")
+
+saludar(nombre="Ana")
+
+""" Funciones con n argumentos (*args) """
+def mostrar_numeros(*numeros):
+    for numero in numeros:
+        print(numero)
+
+# mostrar_numeros(1, "Hola", True)
+
+
+""" Funciones con n argumentos con nombres (**kwargs) """
+def mostrar_info(**info):
+    print(info.items())
+    for clave, valor in info.items():
+        print(f"{clave}: {valor}")
+
+mostrar_info(nombre="Ana", edad=30, correo="ana@gmail.com")
