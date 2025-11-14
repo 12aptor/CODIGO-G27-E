e3e4c005bfb9c9c1cf650f9d98ff20f824ab5c04
12aptor <paholo2054@gmail.com>
2025-11-13 22:51:09 -0500

    introduccion a flask

---
+++ semana03/dia01/flask/README.md
@@ -0,0 +1,16 @@
+# Crear una aplicación Flask 😎
+
+## Crear y activar un entorno virtual
+
+```bash
+python -m venv venv
+venv\Scripts\activate
+source venv/Scripts/activate
+source venv/bin/activate
+```
+
+## Instalar flask
+
+```bash
+pip install flask
+```
---
+++ semana03/dia01/flask/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bienvenido a mi aplicación de Flask 🐍"
+
+@app.route('/html')
+def html():
+    return "<button>Dame click</button>"
+
+@app.route('/json')
+def json():
+    return {"ok": True}
+
+@app.route('/user/<name>')
+def user(name):
+    return f"Hola {name}"
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return f"Post {post_id * 2}"
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    return data
+
+@app.post('/document')
+def document():
+    form = request.form['type']
+    file = request.files['file']
+    print(form, file)
+    return "Documento"
+
+@app.get('/hello')
+def hello():
+    return render_template('hello.html')
+
+@app.get('/search')
+def search():
+    page = request.args.get('page')
+    perPage = request.args.get('perPage')
+    print(page, perPage)
+    return "Search"
+
+if __name__ == '__main__':
+    app.run(debug=True)
